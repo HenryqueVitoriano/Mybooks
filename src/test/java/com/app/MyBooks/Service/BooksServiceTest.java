e76707400a23b005513de9cd16de5a5080ed9d74
HenryqueVitoriano <henryquevitoriano@gmail.com>
2026-07-21 21:30:58 -0300

    feat: learning about Junit and Mock

--- src/test/java/com/app/MyBooks/Service/BooksServiceTest.java
+++ src/test/java/com/app/MyBooks/Service/BooksServiceTest.java
@@ -1,29 +1,58 @@
 package com.app.MyBooks.Service;
 
+import com.app.MyBooks.Model.DTO.LibraryResponse;
 import com.app.MyBooks.Model.Entities.Book;
+import com.app.MyBooks.Repository.BooksRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ActiveProfiles;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
-@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
 class BooksServiceTest {
-    @Autowired
-    BooksService booksService;
+
+
+    @Mock
+    LibraryService libraryService;
+    @Mock
+    BooksRepository booksRepository;
+
+    @InjectMocks
+    private BooksService booksService;
+
 
     @Test
     @DisplayName("Should get a book when everything is OK")
     void createBookSuccessfully() {
-      String isbn = "978-8544001516";
-      Optional<Book> book = Optional.ofNullable(booksService.createBook(isbn));
+        String isbn = "978-8544001516";
+        List<String> authorName = List.of("Machado de assis");
 
-      assertThat(book.isPresent()).isTrue();
+        LibraryResponse response = new LibraryResponse();
+        LibraryResponse responseName = new LibraryResponse();
+
+
+        responseName.setAuthor(authorName);
+
+        response.setTitle("Mémorias postumas de brás cubas");
+        response.setNumberOfPages(345);
+
+        when(libraryService.openLibraryISBN(isbn)).thenReturn(response);
+        when(libraryService.openLibraryNameRequesition(isbn)).thenReturn(responseName);
+
+        Book book = booksService.createBook(isbn);
+
+        assertThat(book).isNotNull();
+
+
     }
-
 
 }
